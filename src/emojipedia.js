476e0484ced20d6b27e14684b346b5e805cf5f7f
pallavimshr <44530247+pallavimshr@users.noreply.github.com>
2023-11-20 22:00:55 +0530

    initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,43 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: "💪",
+      name: "Tense Biceps",
+      meaning:
+        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+      id: 2,
+      emoji: "🙏",
+      name: "Person With Folded Hands",
+      meaning:
+        "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+      id: 3,
+      emoji: "🤣",
+      name: "Rolling On The Floor, Laughing",
+      meaning:
+        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+    {
+      id:4,
+      emoji:"😄",
+      name:"Smiling Face With Open Mouth And Smiling Eyes",
+      meaning: "A smiling face with laughing eyes 😄 represents loud, boisterous and hearty laughter, with the typical laughing emoji eyes that distinguish a real smile from a fake one."
+    } ,
+    {
+      id:5,
+      emoji:"😁",
+      name:"Grinning Face With Smiling Eyes",
+      meaning: "A beaming face and laughing eyes 😁. This emoji shows a mischievous grin that represents lightheartedness and exuberance. It is often used to show that you are happy about an event, excited, or just a little embarrassed. "
+    },
+    {
+      id:6,
+      emoji:"😆",
+      name:"Smiling Face With Open Mouth And Closed Eyes",
+      meaning: "I can't stop laughing! Triggered by incredibly funny or ridiculous situations, the grinning and contorted face emoji 😆 expresses the feeling of being on the verge of a fit of laughter."
+    }
+  ];
+  export default emojipedia;
+  
